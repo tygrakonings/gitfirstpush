2e5e4f44a022b65a4f20d83e5654c9559873cb2d
Tygra Konings <tygra.konings@hotmail.com>
2022-04-22 09:26:57 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let btns = document.querySelectorAll("#book-list .delete");
+
+Array.from(btns).forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const li = e.target.parentElement;
+
+    li.parentNode.removeChild(li);
+  });
+});
+
+const link = document.querySelector("#page-banner a");
+link.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("navigation to", e.target.textContent, "was prevented");
+});
